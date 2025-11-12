1fdcbe8ca6484bcfc5c4fe22173b60222be6a017
Your Name <Your E-Mail>
2025-11-12 17:35:50 +0100

    Aufgabe 8.) Set<Set<Integer>> findAllSums(int[] numbers, int sum), XXL

--- src/main/numbers/Numbers.java
+++ src/main/numbers/Numbers.java
@@ -98,8 +98,7 @@ public interface Numbers {
      * @return instance of the {@link Numbers} interface
      */
     static Numbers getInstance() {
-        throw new UnsupportedOperationException("Unimplemented method 'getInstance()' "
-            + "in interface 'Numbers'. Create an implementation class and return.");
+        return NumbersImpl.getInstance();
     }
 
     /**
